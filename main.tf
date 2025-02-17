c4405293ab281b6e54235abf41c24a518fb75c00
joe <joseph.miller364+github@gmail.com>
2025-02-16 21:29:14 -0700

    Using ASG module

--- main.tf
+++ main.tf
@@ -29,25 +29,29 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                     = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.1.0"
+
+  name      = "blog"
+  min_size  = 1
+  max_size  = 2
+
+  image_id                = data.aws_ami.app_ami.id
   instance_type           = var.instance_type
-  vpc_security_group_ids  = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
+  vpc_zone_identifier   = module.blog_vpc.public_subnets
+  security_groups       = [module.blog_sg.security_group_id]
 }
 
-module "alb" {
+
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "9.13.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
   access_logs = {
