59308f5f11a1008a93281a79fa102f6ba6516507
joe <joseph.miller364+github@gmail.com>
2025-02-17 20:46:53 -0700

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,21 +11,21 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "blog_vpc"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -33,9 +33,9 @@ module "blog" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.1.0"
 
-  name      = "blog"
-  min_size  = 1
-  max_size  = 2
+  name      = "${var.environment.name}-blog"
+  min_size  = var.asg_min_size
+  max_size  = var.asg_max_size
 
   image_id                = data.aws_ami.app_ami.id
   instance_type           = var.instance_type
@@ -43,19 +43,18 @@ module "blog" {
   vpc_zone_identifier   = module.blog_vpc.public_subnets
   security_groups       = [module.blog_sg.security_group_id]
   traffic_source_attachments = {
-    ex-alb = {
-      traffic_source_identifier = module.blog_alb.target_groups["ex-asg"].arn
+    blog_alb = {
+      traffic_source_identifier = module.blog_alb.target_groups["blog_asg"].arn
       traffic_source_type = "elbv2"
     }
   }
 }
 
-
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "9.13.0"
 
-  name            = "blog-alb"
+  name            = "${var.environment.name}-blog-alb"
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -73,7 +72,7 @@ module "blog_alb" {
 
   target_groups = {
     ex-asg = {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
@@ -82,17 +81,16 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
-
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  vpc_id = module.blog_vpc.vpc_id
-  name = "blog_new"
 
+  vpc_id = module.blog_vpc.vpc_id
+  name = "${var.environment.name}-blog"
   ingress_rules         = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
   egress_rules          = ["all-all"]
